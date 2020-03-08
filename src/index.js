a9edc93a6444a42e4ee5af9e8ae9fc00a700a5f2
Prokopenko <alex27prok@gmail.com>
2020-03-09 02:44:33 +0300

    fix: Add function, which find create an array as towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix || !matrix.length)
+    return [];
+
+  var result = new Array();
+  for(var i = 0; i < matrix.length; i++)
+  {
+    if(i % 2 == 0)
+      for( var k = 0; k < matrix[i].length; k++)
+        result.push(matrix[i][k]);
+    else
+      for( var k = matrix[i].length - 1; k >= 0; k--)
+        result.push(matrix[i][k]);
+  }
+
+  return result;
 }
